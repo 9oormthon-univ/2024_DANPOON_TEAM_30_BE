1e1fbf09623c7af92e0d1e849761430e60950cd3
jjh75607 <dkrak5738@naver.com>
2024-11-18 18:40:37 +0900

    feat: security config 추가

---
+++ src/main/java/soon/ready_action/global/oauth2/handler/Oauth2KakaoFailHandler.java
@@ -0,0 +1,21 @@
+package soon.ready_action.global.oauth2.handler;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class Oauth2KakaoFailHandler implements AuthenticationFailureHandler {
+
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
+        AuthenticationException exception) throws IOException, ServletException {
+        log.info("카카오 로그인 실패했습니다. 에러 메시지 : {}", exception.getMessage());
+    }
+}
---
+++ src/main/java/soon/ready_action/global/oauth2/handler/Oauth2KakaoSuccessHandler.java
@@ -0,0 +1,10 @@
+package soon.ready_action.global.oauth2.handler;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class Oauth2KakaoSuccessHandler {
+
+}
