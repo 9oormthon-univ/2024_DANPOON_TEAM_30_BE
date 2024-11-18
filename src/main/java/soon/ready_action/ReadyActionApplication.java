8b5224d38ca1200ebe576682962cac2c27279877
jjh75607 <dkrak5738@naver.com>
2024-11-18 13:29:15 +0900

    setting: project init

---
+++ src/main/java/soon/ready_action/ReadyActionApplication.java
@@ -0,0 +1,13 @@
+package soon.ready_action;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadyActionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReadyActionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/soon/ready_action/ReadyActionApplicationTests.java
@@ -0,0 +1,13 @@
+package soon.ready_action;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadyActionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
