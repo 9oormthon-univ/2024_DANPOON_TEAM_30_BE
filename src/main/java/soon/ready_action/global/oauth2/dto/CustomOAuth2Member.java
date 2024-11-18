97204350882e8330defbb5c5fda8643aeffc06ef
jjh75607 <dkrak5738@naver.com>
2024-11-18 19:50:09 +0900

    feat: customOauth2Member 추가

---
+++ src/main/java/soon/ready_action/global/oauth2/dto/CustomOAuth2Member.java
@@ -0,0 +1,23 @@
+package soon.ready_action.global.oauth2.dto;
+
+import java.util.Collection;
+import java.util.Map;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+import soon.ready_action.domain.member.entity.Role;
+
+@Getter
+public class CustomOAuth2Member extends DefaultOAuth2User {
+
+    private final Role role;
+    private final Long memberId;
+
+    public CustomOAuth2Member(Collection<? extends GrantedAuthority> authorities,
+        Map<String, Object> attributes, String nameAttributeKey, Role role, Long memberId) {
+        super(authorities, attributes, nameAttributeKey);
+
+        this.role = role;
+        this.memberId = memberId;
+    }
+}
--- src/main/java/soon/ready_action/global/oauth2/handler/Oauth2KakaoSuccessHandler.java
+++ src/main/java/soon/ready_action/global/oauth2/handler/Oauth2KakaoSuccessHandler.java
@@ -1,10 +1,32 @@
 package soon.ready_action.global.oauth2.handler;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
+import soon.ready_action.domain.member.entity.Role;
+import soon.ready_action.global.oauth2.dto.CustomOAuth2Member;
+import soon.ready_action.global.oauth2.jwt.provider.TokenProvider;
 
 @Slf4j
+@RequiredArgsConstructor
 @Component
-public class Oauth2KakaoSuccessHandler {
+public class Oauth2KakaoSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
 
+    private final TokenProvider tokenProvider;
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+        Authentication authentication) throws IOException, ServletException {
+        log.info("onAuthenticationSuccess");
+
+        CustomOAuth2Member oAuth2Member = (CustomOAuth2Member) authentication.getPrincipal();
+        Role role = oAuth2Member.getRole();
+        Long memberId = oAuth2Member.getMemberId();
+    }
 }
