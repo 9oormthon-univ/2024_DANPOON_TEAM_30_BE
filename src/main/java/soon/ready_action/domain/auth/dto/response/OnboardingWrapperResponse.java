5f8cf2f354504fd1cf7f72360d6e44ff762b8fc8
jjh75607 <dkrak5738@naver.com>
2024-11-20 15:46:31 +0900

    feat: 스웨거 어노테이션 추가

--- src/main/java/soon/ready_action/domain/auth/dto/response/OnboardingWrapperResponse.java
+++ src/main/java/soon/ready_action/domain/auth/dto/response/OnboardingWrapperResponse.java
@@ -1,15 +1,19 @@
 package soon.ready_action.domain.auth.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.List;
 import lombok.Builder;
 import soon.ready_action.domain.diagnosis.dto.response.OnboardingQuestionResponse;
 import soon.ready_action.global.oauth2.jwt.dto.response.TokenResponse;
 
+@Schema(description = "사용자 관심 카테고리 선택에 따른 response")
 @Builder
 public record OnboardingWrapperResponse(
 
+    @Schema(description = "토큰 response")
     TokenResponse tokenResponse,
 
+    @Schema(description = "관심 카테고리 질문 List")
     List<OnboardingQuestionResponse> onboardingQuestionResponse
 ) {
 
--- src/main/java/soon/ready_action/domain/diagnosis/dto/response/OnboardingQuestionResponse.java
+++ src/main/java/soon/ready_action/domain/diagnosis/dto/response/OnboardingQuestionResponse.java
@@ -1,12 +1,16 @@
 package soon.ready_action.domain.diagnosis.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
+@Schema(description = "관심카테고리 질문 List")
 @Builder
 public record OnboardingQuestionResponse(
 
+    @Schema(description = "질문 내용", example = "정기적으로 신체적, 정신적 관리에 힘쓰며 미래 건강 관리를 위한 계획이 세워져있나요?")
     String content,
 
+    @Schema(description = "카테고리 제목", example = "건강")
     String category
 ) {
 
