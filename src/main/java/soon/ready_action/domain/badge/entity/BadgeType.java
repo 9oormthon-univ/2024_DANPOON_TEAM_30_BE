6b7c2bc0eeb765237184ac6440f3c717a794b034
jjh75607 <dkrak5738@naver.com>
2024-11-22 22:08:28 +0900

    feat: 뱃지 타입 수정

--- src/main/java/soon/ready_action/domain/badge/entity/BadgeType.java
+++ src/main/java/soon/ready_action/domain/badge/entity/BadgeType.java
@@ -1,6 +1,17 @@
 package soon.ready_action.domain.badge.entity;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum BadgeType {
 
-    HEALTH, RESIDENCE, EDUCATION, FINANCE, EMPLOYMENT
+    HEALTH("건강"),
+    RESIDENCE("주거"),
+    EDUCATION("교육"),
+    FINANCE("금융"),
+    EMPLOYMENT("취업");
+
+    private final String type;
 }
