b5cdb63c64bead635bd5be01b0d839637e440df4
jjh75607 <dkrak5738@naver.com>
2024-11-23 16:33:14 +0900

    feat: 스웨거 추가

--- src/main/java/soon/ready_action/domain/diagnosis/dto/response/DiagnosisResultWrapper.java
+++ src/main/java/soon/ready_action/domain/diagnosis/dto/response/DiagnosisResultWrapper.java
@@ -1,17 +1,22 @@
 package soon.ready_action.domain.diagnosis.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.List;
 import lombok.Builder;
 import soon.ready_action.domain.diagnosis.dto.CalculateDiagnosisResult;
 import soon.ready_action.domain.program.dto.response.ProgramResponse.DetailResponse;
 
+@Schema(description = "진단 결과 Wrapper")
 @Builder
 public record DiagnosisResultWrapper(
 
+    @Schema(description = "캐릭터 타입", example = "쿨쿨이")
     String characterType,
 
+    @Schema(description = "진단 결과 리스트")
     List<CalculateDiagnosisResult> results,
 
+    @Schema(description = "프로그램 리스트")
     List<DetailResponse> programs
 ) {
 
